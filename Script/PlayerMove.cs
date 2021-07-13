91e3c337b517d3290fb5e9234099c853ff29f6e9
Roompuppy <86652628+Roompuppy@users.noreply.github.com>
2021-07-14 01:13:42 +0900

    개발 첫날 정글 2스테이지 2021.7.14
    타일, 플레이어 생성 및 이동, 오브젝트 생성 완료
    오브젝트 상호작용 만드는 중
    오브젝트 상호작용 너무 어려워

---
+++ Script/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public bool isDelay;
+    public float delayTime = 2f;
+    Rigidbody2D rigid2D;
+    GameObject waterpipe3;
+
+    void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            if(!isDelay)
+            {
+                isDelay = true;
+
+                float x = Input.GetAxisRaw("Horizontal");
+                float y = Input.GetAxisRaw("Vertical");
+
+                rigid2D.position += new Vector2(x, y);
+                StartCoroutine(CountMoveDelay());
+            }
+        }
+
+    }
+    IEnumerator CountMoveDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        isDelay = false;
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "waterpipe3")
+        {
+            rigid2D.position += new Vector2(5,5);
+            ObMove(collision.transform.position);
+        }
+    }
+    void ObMove(Vector2 targetpos)
+    {
+        float X_cpy = waterpipe3.GetComponent<Transform>().position.x;
+        float Y_cpy = waterpipe3.GetComponent<Transform>().position.y;
+        int dirc_x = waterpipe3.GetComponent<Transform>().position.x - targetpos.x > 0 ? 1 : -1;
+        int dirc_y = waterpipe3.GetComponent<Transform>().position.y - targetpos.y > 0 ? 1 : -1;
+
+        waterpipe3.GetComponent<Transform>().position = new Vector3(X_cpy + dirc_x, Y_cpy + dirc_y, 0f);
+    }
+}
